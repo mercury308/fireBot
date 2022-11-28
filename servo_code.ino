1b6dd20932ceec7feff78d4bcb46f2bd98cdacb6
v3nusindigo <69015092+v3nusindigo@users.noreply.github.com>
2022-11-28 11:55:53 -0500

    Update servo_code.ino

--- servo_code.ino
+++ servo_code.ino
@@ -9,8 +9,8 @@ motor2.attach(6);
 Serial.begin(9600);
 while(Serial.available())
 Serial.read();
-servo1.write(90); 
-servo2.write(90); 
+motor1.write(90); 
+motor2.write(90); 
 Serial.println("INITIALIZING...");
 }
 void loop()
@@ -46,14 +46,14 @@ if ((data[0] == 'L') || (data[0] == 'l')) {
 int firstVal = strtol(data+1, NULL, 10); 
 firstVal = constrain(firstVal,0,180); 
 motor1.write(firstVal);
-Serial.print("Servo1 is set to: ");
+Serial.print("Motor1 is set to: ");
 Serial.println(firstVal);
 }
 if ((data[0] == 'R') || (data[0] == 'r')) {
 int secondVal = strtol(data+1, NULL, 10); 
 secondVal = constrain(secondVal,0,255); 
 motor2.write(secondVal);
-Serial.print("Servo2 is set to: ");
+Serial.print("Motor2 is set to: ");
 Serial.println(secondVal);
 }
 }
