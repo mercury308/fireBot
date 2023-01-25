79c8c4d2b549cd87ba05686795c54d25dc444be2
maddypags <117679076+maddypags@users.noreply.github.com>
2023-01-25 14:03:21 -0500

    Update servo_code.ino

--- servo_code.ino
+++ servo_code.ino
@@ -4,56 +4,60 @@ Servo motor1;
 Servo motor2; 
 void setup()
 {
-motor1.attach(5); 
-motor2.attach(6); 
-Serial.begin(9600);
-while(Serial.available())
-Serial.read();
-motor1.write(90); 
-motor2.write(90); 
-Serial.println("INITIALIZING...");
+  motor1.attach(5); 
+  motor2.attach(6); 
+  Serial.begin(9600);
+
+  while(Serial.available())
+    Serial.read();
+    motor1.write(90); 
+    motor2.write(90); 
+    Serial.println("INITIALIZING...");
 }
-void loop()
-{
-if (Serial.available() > 0) {
-int index=0;
-delay(100); 
-int numChar = Serial.available(); 
-if (numChar>10) {
-numChar=10;
+
+void loop(){
+  if (Serial.available() > 0){
+    int index = 0;
+    delay(100); 
+    int numChar = Serial.available(); 
+  if (numChar>10) {
+    numChar=10;
+  }
+  while (numChar--) {
+    buffer[index++] = Serial.read();
+  }
+  buffer[index]='\0';
+  splitString(buffer);
+  }
 }
-while (numChar--) {
-buffer[index++] = Serial.read();
-}
-buffer[index]='\0';
-splitString(buffer);
-}
-}
+
 void splitString(char* data) {
-Serial.print("Data entered: ");
-Serial.println(data);
-char* parameter;
-parameter = strtok (data, " ,");
-while (parameter != NULL) { 
-setServo(parameter); 
-parameter = strtok (NULL, " ,");
-}
-while(Serial.available())
-Serial.read();
+  
+  Serial.print("Data entered: ");
+  Serial.println(data);
+  char* parameter;
+  parameter = strtok (data, " ,");
+
+  while (parameter != NULL) { 
+    setServo(parameter); 
+    parameter = strtok (NULL, " ,");
+  }
+  while(Serial.available())
+  Serial.read();
 }
 void setServo(char* data) {
-if ((data[0] == 'L') || (data[0] == 'l')) {
-int firstVal = strtol(data+1, NULL, 10); 
-firstVal = constrain(firstVal,0,180); 
-motor1.write(firstVal);
-Serial.print("Motor1 is set to: ");
-Serial.println(firstVal);
-}
-if ((data[0] == 'R') || (data[0] == 'r')) {
-int secondVal = strtol(data+1, NULL, 10); 
-secondVal = constrain(secondVal,0,255); 
-motor2.write(secondVal);
-Serial.print("Motor2 is set to: ");
-Serial.println(secondVal);
-}
+  if ((data[0] == 'L') || (data[0] == 'l')) {
+    int firstVal = strtol(data+1, NULL, 10); 
+    firstVal = constrain(firstVal,0,180); 
+    motor1.write(firstVal);
+    Serial.print("Motor1 is set to: ");
+    Serial.println(firstVal);
+  }
+  if ((data[0] == 'R') || (data[0] == 'r')) {
+  int secondVal = strtol(data+1, NULL, 10); 
+  secondVal = constrain(secondVal,0,255); 
+  motor2.write(secondVal);
+  Serial.print("Motor2 is set to: ");
+  Serial.println(secondVal);
+  }
 }
